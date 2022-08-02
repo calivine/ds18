70de2ede14e7a1c8f634408af56663c0bb79ce7e
Alex Caloggero <a.calog@protonmail.com>
2022-08-02 11:07:13 -0400

    Sensor display; lcd driver

--- temp_sensor.py
+++ temp_sensor.py
@@ -6,5 +6,6 @@ from ds18b20 import DS18B20
 ds18b20 = DS18B20()
 
 while True:
-    print(ds18b20.read_temp_f())
+    temp_f = ds18b20.read_temp_f()
+    print("{}".format(temp_f))
     time.sleep(60)
